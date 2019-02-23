b0e878f3842e6a7ca99f900d2c6614afb8e76108
cyberboysumanjay <cyberboysumanjay@gmail.com>
2019-02-23 13:25:06 +0530

    Updating json files

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
+import time
+import getproxy
+import json,requests,urllib.request
+from traceback import print_exc
+from ast import literal_eval
+import pprint
+
 def push_to_git():
     #Import dependencies
     from subprocess import call
@@ -9,45 +16,49 @@ def push_to_git():
     call('git commit -m "'+ commit_message +'"', shell = True)
     #Push the new or update files
     call('git push origin master', shell = True)
-import json,requests,urllib.request
-from traceback import print_exc
 
 def get_proxy():
     proxies=[]
-    i,c=0,0
-    while(c!=10):
-        try:
-            url = 'http://pubproxy.com/api/proxy?country=IN&limit=20&https=True&user_agent=true'
-            resp = requests.get(url=url)
-            data = resp.json()
-            pprint.pprint(data)
-            for proxy in data['data']:
-                proxies.append(proxy['ipPort'])
+    try:
+        user_agent = {'User-agent': 'Mozilla/5.0'}
+        url = 'http://pubproxy.com/api/proxy?country=IN&limit=20&https=True&user_agent=true'
+        resp = requests.get(url=url,headers=user_agent)
+        data = resp.json()
+        time.sleep(3)
+        for proxy in data['data']:
+            proxies.append(proxy['ipPort'])
 
-            #print('Direct Pubproxy accessed')
+    except Exception as e:
+        i=1
+        try:
+            proxies=proxies+spy_proxy()
         except Exception as e:
-            print(e.__class__.__name__)
-            print_exc()
-            i=1
-            try:
-                proxies=proxies+spy_proxy()
-                #print('Spy Proxy accessed')
-            except Exception as e:
-                print(e)
-                exit()
-        c=c+1
-        print(str(c)+'th Attempt taken')
+            proxies=proxies+fate_proxy()
     return proxies
 
-import pprint
-#print((get_proxy()))
+def fate_proxy():
+    resp=requests.get('https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list')
+    a=((resp.text).split('\n'))
+    p_list=[]
+    for i in a:
+        try:
+            p_list.append(json.loads(i))
+        except Exception as e:
+            continue
+    np_list=[]
+    for i in p_list:
+        if i['country']=='IN':
+            np_list.append(i)
+    #print(len(np_list))
+    proxy=[]
+    for i in np_list:
+        proxy.append((str(i['host'])+':'+str(i['port'])))
+    return(proxy)
+
 def spy_proxy():
     resp=requests.get('http://spys.me/proxy.txt')
-
-    #print(resp.text)
     data=((resp.text).split("Google_passed(+)")[1]).split('\r')[0]
     data=data.split('\n')
-    #print(data)
     l=[]
     for i in data:
         if 'IN' in i:
@@ -55,8 +66,8 @@ def spy_proxy():
             l.append(i)
     return l
 
-proxy_json={'data':get_proxy()}
 
+proxy_json={'data':get_proxy()}
 import json
 with open('proxy.json', 'w') as outfile:
     json.dump(proxy_json, outfile)
