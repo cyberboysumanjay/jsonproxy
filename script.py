61337e13bab220b6cbea67c812c6f4eb80e60d14
cyberboysumanjay <cyberboysumanjay@gmail.com>
2019-03-01 01:14:22 +0530

    Updating json files

--- script.py
+++ script.py
@@ -5,6 +5,25 @@ from traceback import print_exc
 from ast import literal_eval
 import pprint
 
+def set_proxy():
+    resp=requests.get('https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list')
+    a=((resp.text).split('\n'))
+    p_list=[]
+    for i in a:
+        try:
+            p_list.append(json.loads(i))
+        except Exception as e:
+            continue
+    np_list=[]
+    for i in p_list:
+        np_list.append(i)
+    #print(len(np_list))
+    proxy=[]
+    for i in np_list:
+        proxy.append((str(i['host'])+':'+str(i['port'])))
+    print(len(proxy))
+    return(proxy)
+
 def push_to_git():
     #Import dependencies
     from subprocess import call
@@ -20,15 +39,31 @@ def push_to_git():
 def get_proxy():
     proxies=[]
     user_agent = {'User-agent': 'Mozilla/5.0'}
+    '''
     proxy_url='http://sumanjay.ooo/jsonproxy/proxy.json'
     resp = requests.get(url=proxy_url,headers=user_agent)
     proxy=resp.json()
     proxy=proxy['data']
+    '''
+    proxy=set_proxy()
     i,j=0,0
+
+    try:
+        print("Entered Spy proxy")
+        proxies=proxies+spy_proxy()
+        print("Length of spy Proxy is ",len(proxies))
+    except Exception :
+        pass
+    try:
+        print('Entered Fate Proxy')
+        proxies=proxies+fate_proxy()
+        print("Length of fate Proxy is ",len(proxies))
+    except Exception:
+        pass
 
     for p in proxy:
         url = 'http://pubproxy.com/api/proxy?country=IN&limit=20&https=True&user_agent=true'
-        while(len(proxies)<1500):
+        while(len(proxies)<5000):
             try:
                 j=0
                 resp = requests.get(url=url,headers=user_agent,proxies={"http": p, "https": p})
@@ -45,15 +80,7 @@ def get_proxy():
             finally:
                 i=i+1
 
-    try:
-        print("Entered Spy proxy")
-        proxies=proxies+spy_proxy()
-    except Exception :
-        pass
-    try:
-        proxies=proxies+fate_proxy()
-    except Exception:
-        pass
+
     return proxies
 
 def fate_proxy():
@@ -85,7 +112,6 @@ def spy_proxy():
             i=i.split(' IN')[0]
             l.append(i)
     return l
-
 
 proxy_json={'data':get_proxy()}
 import json
