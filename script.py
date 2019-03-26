1d5da0c99f39f2b5ed6249e48859ac0c39b5da33
cyberboysumanjay <cyberboysumanjay@gmail.com>
2019-03-27 01:32:39 +0530

    Updated Script

--- script.py
+++ script.py
@@ -39,7 +39,7 @@ def push_to_git():
     #Commit Message
     india = timezone('Asia/Kolkata')
     in_time = datetime.now(india)
-    commit_message= ("Updated at "+ str(in_time.strftime('%H-%M-%S'))+"IST")
+    commit_message= ("Updated at "+ str(in_time.strftime('%H-%M-%S'))+" IST")
     #Stage the file
     call('git add .', shell = True)
     # Add your commit
