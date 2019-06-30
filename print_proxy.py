78f5de84e0322388df98a7a5c414a462b925239d
cyberboysumanjay <cyberboysumanjay@gmail.com>
2019-06-30 13:50:07 +0530

    Updated Proxy Domain

--- print_proxy.py
+++ print_proxy.py
@@ -1,4 +1,4 @@
 import requests,json
-r=requests.get('http://sumanjay.ooo/jsonproxy/proxy.json')
+r=requests.get('https://cyberboysumanjay.github.io/jsonproxy/proxy.json')
 r_json=r.json()
 print(len(r_json['data']))
